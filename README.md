be0476b077d03e6b42e794e20a31ab30d53e853a
youtonit <106815948+youtonit@users.noreply.github.com>
2022-06-23 12:56:58 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# First version of Dungeon_Console
+
+# A simple console game Dungeon in console
+
+## To start a game on Linux/MacOs/BSD:
+``g++ main.cpp -o game``
+``./game``
+
+## To start a game on Windows:
+``g++ main.cpp -o game.exe``
+``Start it``
---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Stats {
+public:
+    int monster_health;
+    int monster_damage;
+    int hero_health;
+    int hero_damage;
+    string monster1_name;
+    string monster2_name;
+    string result;
+
+    void operation() {
+        monster_health = monster_health - hero_damage;
+        hero_health = hero_health - monster_damage;
+    }
+
+};
+
+void attack_unga(Stats* stat) {
+  stat->operation();
+
+        if (stat->hero_health == 0) {
+            stat->result = "GAME OVER";
+            cout << stat->result;
+            return;
+        }
+        if (stat->monster_health == 0) {
+            stat->result = "GREAT!";
+            cout << stat->result;
+            return;
+        }
+        if (stat->hero_health < 100) {
+            cout << "YES, YOU DID THIS! ";
+            cout << "OH NO, YOUR HP IS " << stat->hero_health << " BE NEAT ";
+            return;
+    }
+};
+
+
+int main() {
+    Stats stat;
+    stat.monster_health = 100;
+    stat.monster_damage = 15;
+    stat.hero_health = 100;
+    stat.hero_damage = 20;
+    stat.result;
+
+    stat.operation();
+    while (true) {
+        cout << "\n 1. Attack! ";
+        string choose;
+        cin >> choose;
+
+        while (choose == "1") {
+            attack_unga(&stat);
+            break;
+        }
+
+        if (stat.result == "GAME OVER" or stat.result == "GREAT!") {
+            break;
+        }
+
+    }
+
+  return 0;
+}
